c31356fb3e506728c969d0c7cbd71e0294b9048d
Christopher Roemheld <chris.roemheld@tum.de>
2017-09-10 15:26:27 +0200

    Refactored include.c to follow kernel coding style

--- include.c
+++ include.c
@@ -5,22 +5,20 @@
 void **table_ptr = NULL;
 
 /* check if list is empty */
-int is_empty_data_node(struct data_node **head)  {
-
+int is_empty_data_node(struct data_node **head)
+{
 	return *head == NULL;
 }
 
 /* special case of find_data_node_field (with offset = 0) */
-struct data_node *find_data_node(struct data_node **head, void *data, int length) {
-
+struct data_node *find_data_node(struct data_node **head, void *data, 
+	int length)
+{
 	struct data_node *node = *head;
 
 	while(node != NULL) {
-
 		if(!memcmp(data, node->data, length)) {
-
 			debug("DATA_NODE FOUND IN LIST");
-
 			return node;
 		}
 
@@ -30,16 +28,14 @@ struct data_node *find_data_node(struct data_node **head, void *data, int length
 	return NULL;
 }
 
-struct data_node *find_data_node_field(struct data_node **head, void *needle, int offset, int length) {
-
+struct data_node *find_data_node_field(struct data_node **head, void *needle, 
+	int offset, int length) 
+{
 	struct data_node *node = *head;
 
 	while(node != NULL) {
-
 		if(!memcmp(node->data + offset, needle, length)) {
-
 			debug("DATA_NODE FOUND IN LIST");
-
 			return node;
 		}
 
@@ -50,124 +46,96 @@ struct data_node *find_data_node_field(struct data_node **head, void *needle, in
 }
 
 /* insert node at the beginning of the current list */
-struct data_node *insert_data_node(struct data_node **head, void *data) {
-
-	/* allocate node */
+struct data_node *insert_data_node(struct data_node **head, void *data)
+{
 	struct data_node *node = kmalloc(sizeof(struct data_node), GFP_KERNEL);
-
-	/* put in the data  */
 	node->data = data;
 
 	/* since we are adding at the begining, prev is always NULL */
 	node->prev = NULL;
-
-	/* link the old list off the new node */
 	node->next = (*head);    
 
-	/* change prev of head node to new node */
-	if((*head) != NULL) {
+	if((*head) != NULL)
+		(*head)->prev = node;
 
-		(*head)->prev = node;    
-	}
-
-	/* move the head to point to the new node */
 	(*head) = node;
 
 	return (*head);
 }
 
 /* delete node */
-void delete_data_node(struct data_node **head, struct data_node *node) {
-
+void delete_data_node(struct data_node **head, struct data_node *node)
+{
 	/* check for NULL */
 	if(*head == NULL || node == NULL) {
-
 		debug("DATA_NODE LIST EMPTY");
-
 		return;
 	}
 
 	/* node to be deleted is head */
 	if(*head == node) {
-
 		debug("DATA_NODE IS HEAD")
-
 		*head = node->next;	
 	}
 
 	/* next node */
-	if(node->next != NULL) {
-
+	if(node->next != NULL)
 		node->next->prev = node->prev;	
-	}
 
 	/* prev node */
-	if(node->prev != NULL) {
-
+	if(node->prev != NULL)
 		node->prev->next = node->next;     	
-	}
 
 	kfree(node);
 }
 
 /* call a function for each node */
-void foreach_data_node_callback(struct data_node **head, void (*callback_function)(struct data_node *)) {
-
+void foreach_data_node_callback(struct data_node **head, 
+	void (*callback_function)(struct data_node *)) 
+{
 	struct data_node *node = *head;
 
 	if(is_empty_data_node(head)) {
-
 		debug("DATA_NODE LIST EMPTY");
-
 		return;
 	}
 
 	while(node != NULL) {
-
 		debug("CALLBACK FUNCTION CALLED");
-
 		callback_function(node);
-
 		node = node->next;
 	}
 }
 
 /* completely free a list */
-void free_data_node_list(struct data_node **head) {
-
+void free_data_node_list(struct data_node **head)
+{
 	if(is_empty_data_node(head)) {
-
 		debug("DATA_NODE LIST EMPTY");
-
 		return;
 	}
 
 	while(!is_empty_data_node(head)) {
-
 		debug("DELETE DATA_NODE");
-
 		delete_data_node(head, *head);
 	}
 }
 
 /* completely free a list with callback function */
-void free_data_node_list_callback(struct data_node **head, void (*callback_function)(struct data_node *)) {
+void free_data_node_list_callback(struct data_node **head, 
+	void (*callback_function)(struct data_node *))
+{
 
 	if(is_empty_data_node(head)) {
-
 		debug("DATA_NODE LIST EMPTY");
-
 		return;
 	}
 
 	while(!is_empty_data_node(head)) {
-
 		debug("CALLBACK FUNCTION CALLED");
 
 		/* callback function with param node */
 		callback_function(*head);
-
-		/* delete node */
 		delete_data_node(head, *head);
 	}
 }
@@ -175,12 +143,10 @@ void free_data_node_list_callback(struct data_node **head, void (*callback_funct
 /* critical section functions (mutexes) */
 
 /* increment counter of a critical section */
-void inc_critical(struct mutex *lock, int *counter) {
-
+void inc_critical(struct mutex *lock, int *counter)
+{
 	/* lock access mutex */
 	mutex_lock(lock);
-
-	/* icrease function call counter */
 	(*counter)++;
 
 	/* unlock access mutex */
@@ -188,12 +154,11 @@ void inc_critical(struct mutex *lock, int *counter) {
 }
 
 /* decrement counter of a critical section */
-void dec_critical(struct mutex *lock, int *counter) {
+void dec_critical(struct mutex *lock, int *counter)
+{
 
 	/* lock access mutex */
 	mutex_lock(lock);
-
-	/* decrease function call counter */
 	(*counter)--;
 
 	/* unlock access mutex */
@@ -203,59 +168,50 @@ void dec_critical(struct mutex *lock, int *counter) {
 /* simple util functions */
 
 /* disable page protection */
-void disable_page_protection(void) {
-
+void disable_page_protection(void)
+{
 	alert("DISABLE_PAGE_PROTECTION");
-
 	write_cr0(read_cr0() & (~0x10000));
 }
 
 /* enable page protection */
-void enable_page_protection(void) {
-
+void enable_page_protection(void)
+{
 	alert("ENABLE_PAGE_PROTECTION");
-
 	write_cr0(read_cr0() | 0x10000);
 }
 
 /* set pointer to system call table */
-int set_sys_call_table(void) {
-
+int set_sys_call_table(void)
+{
 	table_ptr = (void **)get_sys_call_table();
 
-	if(table_ptr == NULL) {
-
+	if(table_ptr == NULL)
 		return 1;
-	}
 
 	return 0;
 }
 
 /* strtoint: convert char *str containing a number into an integer */
-int strtoint(char *str) {
-
+int strtoint(char *str)
+{
 	long res;
 
 	/* using kstrtol with base 10 (decimal) */
-	if(kstrtol(str, 10, &res) == 0) {
+	if(kstrtol(str, 10, &res) == 0)
 		return (int)res;
-	}
 
 	return -1;
 }
 
 /* getdigits: get number of digits for a given int */
-int getdigits(int num) {
-
-	if(num < 0) {
-
+int getdigits(int num)
+{
+	if(num < 0)
 		return 1 + getdigits(num * (-1));
-	}
 
-	if(num < 10) {
-
+	if(num < 10)
 		return 1;
-	}
 
 	return 1 + getdigits(num / 10);
 }
