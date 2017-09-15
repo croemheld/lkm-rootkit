036913fa1934af2a3a05349817b7366a99440c72
Christopher Roemheld <chris.roemheld@tum.de>
2017-09-15 17:38:28 +0200

    Deleted old files

--- headers/core.h
+++
@@ -1,8 +0,0 @@
-#ifndef CORE_H
-#define CORE_H
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Christopher Roemheld");
-MODULE_DESCRIPTION("A loadable kernel module for most versions.");
-
-#endif
--- libs/headers/syscalltable.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSCALLTABLE_H
-#define SYSCALLTABLE_H
-
-#include <linux/types.h>
-#include <asm/msr-index.h>
-
-unsigned long **get_sys_call_table(void);
-
-#endif
